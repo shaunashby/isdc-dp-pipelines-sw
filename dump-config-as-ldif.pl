91ec6c0baccba8813d87927739649ec660518d9b
Shaun Ashby <Shaun.Ashby@unige.ch>
2011-02-15 15:11:51 +0100

    Add script to dump the config as LDIF. Check in current example.

---
+++ dump-config-as-ldif.pl
@@ -0,0 +1,23 @@
+#!/opt/local/bin/perl
+#____________________________________________________________________ 
+# File: dump-config-as-ldif.pl
+#____________________________________________________________________ 
+#  
+# Author: Shaun ASHBY <Shaun.Ashby@gmail.com>
+# Update: 2011-02-15 15:03:32+0100
+# Revision: $Id$ 
+#
+# Copyright: 2011 (C) Shaun ASHBY
+#
+#--------------------------------------------------------------------
+use warnings;
+use strict;
+
+use ISDC::DataProcessing::Pipeline;
+
+for my $pipeline (qw( nrtinput nrtscw nrtrev nrtqla adp consinput consscw consrev ) ) {
+    my $obj = ISDC::DataProcessing::Pipeline->new({ name => "$pipeline", config => "t/pipelines-test-config.yml" });
+    print $obj->to_ldif(),"\n";    
+}
+
+
